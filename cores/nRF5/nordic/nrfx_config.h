1bddcdd9f15ba150da338bf588a80307569228d2
yarbschris <123770805+yarbschris@users.noreply.github.com>
2025-12-17 11:45:34 -0600

    Enable timer, ppi, and gpiote

--- cores/nRF5/nordic/nrfx_config.h
+++ cores/nRF5/nordic/nrfx_config.h
@@ -1,18 +1,18 @@
 #ifndef NRFX_CONFIG_H__
 #define NRFX_CONFIG_H__
 
-#define NRFX_POWER_ENABLED              1
-#define NRFX_POWER_DEFAULT_CONFIG_IRQ_PRIORITY  7
+#define NRFX_POWER_ENABLED 1
+#define NRFX_POWER_DEFAULT_CONFIG_IRQ_PRIORITY 7
 
 #define NRFX_CLOCK_ENABLED 0
 
-#define NRFX_SPIM_ENABLED            1
-#define NRFX_SPIM_MISO_PULL_CFG      1 // pulldown
-#define NRFX_SPIM_EXTENDED_ENABLED   0
+#define NRFX_SPIM_ENABLED 1
+#define NRFX_SPIM_MISO_PULL_CFG 1 // pulldown
+#define NRFX_SPIM_EXTENDED_ENABLED 0
 
-#define NRFX_SPIM0_ENABLED           0 // used as I2C
-#define NRFX_SPIM1_ENABLED           0 // used as I2C
-#define NRFX_SPIM2_ENABLED           1
+#define NRFX_SPIM0_ENABLED 0 // used as I2C
+#define NRFX_SPIM1_ENABLED 0 // used as I2C
+#define NRFX_SPIM2_ENABLED 1
 
 #define NRFX_SPIS_ENABLED 0
 #define NRFX_SPIS_DEFAULT_CONFIG_IRQ_PRIORITY 7
@@ -31,21 +31,28 @@
 #define NRFX_PWM2_ENABLED 0
 #define NRFX_PWM3_ENABLED 0
 
-#define NRFX_TIMER_ENABLED 0
-#define NRFX_TIMER0_ENABLED 0
+// Enable GPIOTE for pcnt
+#define NRFX_GPIOTE_ENABLED 1
+
+// Enable PPI for pcnt
+#define NRFX_PPI_ENABLED 1
+
+// Enable timer peripheral for pcnt
+#define NRFX_TIMER_ENABLED 1
+#define NRFX_TIMER0_ENABLED 1
 #define NRFX_TIMER1_ENABLED 0
 #define NRFX_TIMER2_ENABLED 0
 #define NRFX_TIMER3_ENABLED 0
 
 #if defined(NRF52840_XXAA)
-  #define NRFX_QSPI_ENABLED   1
-  #define NRFX_SPIM3_ENABLED  1
+#define NRFX_QSPI_ENABLED 1
+#define NRFX_SPIM3_ENABLED 1
 #elif defined(NRF52833_XXAA)
-  #define NRFX_QSPI_ENABLED   0
-  #define NRFX_SPIM3_ENABLED  1
+#define NRFX_QSPI_ENABLED 0
+#define NRFX_SPIM3_ENABLED 1
 #else
-  #define NRFX_QSPI_ENABLED   0
-  #define NRFX_SPIM3_ENABLED  0
+#define NRFX_QSPI_ENABLED 0
+#define NRFX_SPIM3_ENABLED 0
 #endif
 
 // NRFX temp
